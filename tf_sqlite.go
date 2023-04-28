a5340704d940cf11d11e3597b6db91b6e5e3795a
andyxbchen <andyxbchen@tencent.com>
2023-04-28 17:28:08 +0800

    update sqlite DB

--- tf_sqlite.go
+++ tf_sqlite.go
@@ -3,37 +3,34 @@ package main
 import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"time"
 )
 
-type Product struct {
+type TencentcloudAlibabacloudProduct struct {
 	gorm.Model
-	Code  string
-	Price uint
+	tencentcloudProductName string
+	alibabacloudProductName string
+	UpdateTime              time.Time
+}
+type TencentcloudAlibabacloudResource struct {
+	gorm.Model
+	alibabacloudResource string
+	tencentcloudResource string
+	sourcesType          string
+	tencentcloudTfUrl    string
+	UpdateTime           time.Time
 }
 
-func main1() {
+func getTencentcloudAlibabacloudProduct() []TencentcloudAlibabacloudProduct {
+	var products []TencentcloudAlibabacloudProduct
+	getDB().Select(&products, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+	return products
+}
+
+func getDB() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
-
-	// 迁移 schema
-	db.AutoMigrate(&Product{})
-
-	// Create
-	db.Create(&Product{Code: "D42", Price: 100})
-
-	// Read
-	var product Product
-	db.First(&product, 1)                 // 根据整型主键查找
-	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
-
-	// Update - 将 product 的 price 更新为 200
-	db.Model(&product).Update("Price", 200)
-	// Update - 更新多个字段
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-
-	// Delete - 删除 product
-	db.Delete(&product, 1)
+	return db
 }
